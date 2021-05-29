67407049993a270d28b3ca437afc755e594bea21
AWINAS-KANNAN-2719 <awinas270597@gmail.com>
2021-05-29 16:51:51 +0530

    Pushing to branch

--- src/main/java/in/lowes/urlshortner/exception/InvalidUrlException.java
+++ src/main/java/in/lowes/urlshortner/exception/InvalidUrlException.java
@@ -7,6 +7,7 @@ public class InvalidUrlException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = -8493273395262797574L;
 
+	
 	public InvalidUrlException() {
 		super();
 	}
